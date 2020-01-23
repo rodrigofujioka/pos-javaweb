42c3e27ea62473baeaf99616ec8c3a14b07381f3
Rodrigo Fujioka <rodrigofujioka@gmail.com>
2020-01-22 22:32:51 -0300

    Ajustando código deprecated no projeto pós web

--- src/main/java/dev/fujioka/java/avancado/web/config/WebConfig.java
+++ src/main/java/dev/fujioka/java/avancado/web/config/WebConfig.java
@@ -1,15 +1,13 @@
 package dev.fujioka.java.avancado.web.config;
 
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer  {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
